79fd7e8e7951bc9e786aaf4acfa03e61f4d0e9e6
semiha <semihagorguluarslan@gmail.com>
2021-11-06 20:36:57 +0300

    meal detail

--- src/components/MealDetail.vue
+++ src/components/MealDetail.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    {{ meal }}
+    <div>
+      <b>Ingredients</b> <br />
+      {{ ingredients }}
+    </div>
+    <div>
+      <app-meal :m="meal"></app-meal>
+    </div>
+    <div>
+      <b>Instructions</b>
+      <br />
+      {{ meal.strInstructions }}
+    </div>
   </div>
 </template>
 
 <script>
+import Meal from "./Meal.vue";
 export default {
+  components: {
+    appMeal: Meal,
+  },
   data() {
     return {
       meal: null,
     };
+  },
+  computed: {
+    ingredients() {
+      let ingArray = [];
+      if (this.meal != null) {
+        for (let i = 1; i <= 20; i++) {
+          let ing = this.meal["strIngredient" + i];
+          if (ing == "" || ing == null) break;
+          ingArray.push(ing);
+        }
+      }
+      return ingArray.join(",");
+    },
   },
   methods: {
     async GetIdMeals(id) {
